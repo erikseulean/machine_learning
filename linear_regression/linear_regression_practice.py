d29bdc2d856290b01c09652518040bebc6ec25cb
eseulean <eseulean@bloomberg.net>
2017-10-25 14:56:38 +0100

    linear regression one variable

---
+++ linear_regression/linear_regression_practice.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+iterations = 1500
+alpha = 0.01
+
+def read_data():
+    data = np.loadtxt('restaurant_profit.txt', delimiter=',')
+    X = data[:, 0]
+    y = data[:, 1]
+
+    return X, y
+
+def plot(X, y):
+    plt.scatter(X, y, marker='x')
+    plt.xlabel("Population of city in 10.000s")
+    plt.ylabel("Profit in $10.000s")
+    plt.show()
+
+
+def plot_best_fit(X, y, theta):
+
+    plt.scatter(X, y, marker='x')
+    plt.xlabel("Population of city in 10.000s")
+    plt.ylabel("Profit in $10.000s")
+
+    plt.plot(X, theta[0] + theta[1] * X)
+    plt.show()
+
+def gradient_descent(X, y, nr_iterations):
+
+    m = X.shape[0]
+    theta = np.zeros(2)
+    for _ in range(nr_iterations):
+        theta_zero = theta[0] - alpha/m * sum(theta[0] + theta[1] * X - y)
+        theta_one = theta[1] - alpha/m * sum((theta[0] + theta[1] * X  - y) * X)
+
+        theta[0] = theta_zero
+        theta[1] = theta_one
+
+    return theta
+
+
+X, y = read_data()
+#plot(X, y)
+plot_best_fit(X, y, gradient_descent(X, y, iterations))
